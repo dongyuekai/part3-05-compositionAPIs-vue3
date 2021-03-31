37f4486583d9091482ab384cfab3b473c2a9eefb
“dongyuekai” <457545648@qq.com>
2021-03-31 16:25:13 +0800

    响应式原理之reactive、依赖收集、effect、track、trigger

---
+++ 05-reactivity/reactivity/index.js
@@ -0,0 +1,74 @@
+
+const isObject = target => (target !== null) && (typeof target === 'object')
+const convert = target => isObject(target) ? reactive(target) : target
+const hasOwnProperty = Object.prototype.hasOwnProperty
+const hasOwn = (target, key) => hasOwnProperty.call(target, key)
+
+export function reactive(target) {
+  if (!isObject(target)) return target
+
+  const handler = {
+    get(target, key, receiver) {
+      // 收集依赖
+      track(target, key)
+      const result = Reflect.get(target, key, receiver)
+      // 如果result是对象 就继续执行reactive
+      return convert(result)
+    },
+    set(target, key, value, receiver) {
+      const oldValue = Reflect.get(target, key, receiver)
+      let result = true
+      if (oldValue !== value) {
+        result = Reflect.set(target, key, value, receiver)
+        // 触发更新
+        trigger(target, key)
+      }
+      return result
+    },
+    deleteProperty(target, key) {
+      const hadKey = hasOwn(target, key)
+      const result = Reflect.deleteProperty(target, key)
+      if (hadKey && result) {
+        // 触发更新
+        trigger(target, key)
+      }
+      return result
+    }
+  }
+
+  return new Proxy(target, handler)
+}
+
+let activeEffect = null
+export function effect(callback) {
+  activeEffect = callback
+  callback() // 访问响应式对象属性 去收集依赖
+  activeEffect = null
+}
+
+// 依赖收集
+let targetMap = new WeakMap()
+export function track(target, key) {
+  if (!activeEffect) return
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()))
+  }
+  let dep = depsMap.get(key)
+  if (!dep) {
+    depsMap.set(key, (dep = new Set()))
+  }
+  dep.add(activeEffect)
+}
+
+// 触发更新
+export function trigger(target, key) {
+  const depsMap = targetMap.get(target)
+  if (!depsMap) return
+  const dep = depsMap.get(key)
+  if (dep) {
+    dep.forEach(effect => {
+      effect()
+    });
+  }
+}
