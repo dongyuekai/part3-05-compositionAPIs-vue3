618f35b83d7811643d4b2c2380f8f13396e3a8ea
“dongyuekai” <457545648@qq.com>
2021-04-01 11:37:18 +0800

    手写一个my-vite-cli 替换原来vite命令（实现单文件组件.vue的编译）

--- 06-vite-cli/index.js
+++ 06-vite-cli/index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 const path = require('path')
-// const { Readable } = require('stream')
+const { Readable } = require('stream')
 const Koa = require('koa')
 const send = require('koa-send')
-// const compilerSFC = require('@vue/compiler-sfc')
+const compilerSFC = require('@vue/compiler-sfc')
 
 const app = new Koa()
 
@@ -14,24 +14,24 @@ const streamToString = stream => new Promise((resolve, reject) => {
   stream.on('error', reject)
 })
 
-// const stringToStream = text => {
-//   const stream = new Readable()
-//   stream.push(text)
-//   stream.push(null)
-//   return stream
-// }
+const stringToStream = text => {
+  const stream = new Readable()
+  stream.push(text)
+  stream.push(null)
+  return stream
+}
 
 // 3. 加载第三方模块
-// app.use(async (ctx, next) => {
-//   // ctx.path --> /@modules/vue
-//   if (ctx.path.startsWith('/@modules/')) {
-//     const moduleName = ctx.path.substr(10)
-//     const pkgPath = path.join(process.cwd(), 'node_modules', moduleName, 'package.json')
-//     const pkg = require(pkgPath)
-//     ctx.path = path.join('/node_modules', moduleName, pkg.module)
-//   }
-//   await next()
-// })
+app.use(async (ctx, next) => {
+  // ctx.path --> /@modules/vue -> ./node_modules/xxx
+  if (ctx.path.startsWith('/@modules/')) {
+    const moduleName = ctx.path.substr(10)
+    const pkgPath = path.join(process.cwd(), 'node_modules', moduleName, 'package.json')
+    const pkg = require(pkgPath)
+    ctx.path = path.join('/node_modules', moduleName, pkg.module)
+  }
+  await next()
+})
 
 // 1. 静态文件服务器
 app.use(async (ctx, next) => {
@@ -40,29 +40,30 @@ app.use(async (ctx, next) => {
 })
 
 // 4. 处理单文件组件
-// app.use(async (ctx, next) => {
-//   if (ctx.path.endsWith('.vue')) {
-//     const contents = await streamToString(ctx.body)
-//     const { descriptor } = compilerSFC.parse(contents)
-//     let code
-//     if (!ctx.query.type) {
-//       code = descriptor.script.content
-//       // console.log(code)
-//       code = code.replace(/export\s+default\s+/g, 'const __script = ')
-//       code += `
-//       import { render as __render } from "${ctx.path}?type=template"
-//       __script.render = __render
-//       export default __script
-//       `
-//     } else if (ctx.query.type === 'template') {
-//       const templateRender = compilerSFC.compileTemplate({ source: descriptor.template.content })
-//       code = templateRender.code
-//     }
-//     ctx.type = 'application/javascript'
-//     ctx.body = stringToStream(code)
-//   }
-//   await next()
-// })
+app.use(async (ctx, next) => {
+  // .vue类型的单文件组件
+  if (ctx.path.endsWith('.vue')) {
+    const contents = await streamToString(ctx.body)
+    const { descriptor } = compilerSFC.parse(contents)
+    let code
+    if (!ctx.query.type) {
+      code = descriptor.script.content
+      // console.log(code)
+      code = code.replace(/export\s+default\s+/g, 'const __script = ')
+      code += `
+      import { render as __render } from "${ctx.path}?type=template"
+      __script.render = __render
+      export default __script
+      `
+    } else if (ctx.query.type === 'template') {
+      const templateRender = compilerSFC.compileTemplate({ source: descriptor.template.content })
+      code = templateRender.code
+    }
+    ctx.type = 'application/javascript'
+    ctx.body = stringToStream(code)
+  }
+  await next()
+})
 
 // 2. 修改第三方模块的路径
 app.use(async (ctx, next) => {
@@ -72,9 +73,11 @@ app.use(async (ctx, next) => {
     // import App from './App.vue  => import App from './App.vue'  不做匹配
     ctx.body = contents
       .replace(/(from\s+['"])(?![\.\/])/g, '$1/@modules/')
-      // .replace(/process\.env\.NODE_ENV/g, '"development"')
+    .replace(/process\.env\.NODE_ENV/g, '"development"')  // 因为在浏览器中没有process.env.NODE_ENV这个变量 所以全局替换为development
   }
 })
+
+
 
 app.listen(3000)
 console.log('Server running @ http://localhost:3000')
